64ad576ed51dc45c5ee50aa7d5ebefdbbba0f9b4
opli <gaggersgag@gmail.com>
2023-02-01 23:42:50 +0000

    completed ball wall collision

--- Engine/Ball.cpp
+++ Engine/Ball.cpp
@@ -23,23 +23,27 @@ bool Ball::wallCollision(const Rect& wall)
 {
 	bool collided = false;
 	const Rect rect = getRect();
-	if (rect.left < wall.left)
+	if (rect.left <= wall.left)
 	{
+		pos.x += wall.left - rect.left;
 		reboundX();
 		collided = true;
 	}
-	else if (rect.right > wall.right)
+	else if (rect.right >= wall.right)
 	{
-		reboundX(); 
+		pos.x -= rect.right - wall.right;
+		reboundX();
 		collided = true;
 	}
-	if (rect.top < wall.top)
+	if (rect.top <= wall.top)
 	{
+		pos.y += wall.top - rect.top;
 		reboundY();
 		collided = true;
 	}
 	else if (rect.bottom >= wall.bottom)
 	{
+		pos.y -= rect.bottom - wall.bottom;
 		reboundY();
 		collided = true;
 	}
